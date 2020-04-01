91101a2072c3ed21cdc8c2967919f2c33ceb20b4
Nirmalya Ghosh <nirmalya.email@gmail.com>
2020-04-02 01:15:17 +0530

    refactor: Convert a part of authentication app to TS

--- packages/authentication/app.ts
+++ packages/authentication/app.ts
@@ -2,47 +2,47 @@
  * Module dependencies.
  */
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const passport = require('passport')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const cors = require("cors");
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-require('dotenv').config()
+require("dotenv").config();
 
 /**
  * Controllers (route handlers).
  */
-const app = express()
+const app = express();
 
-const userController = require('./controllers/user')
+const userController = require("./controllers/user");
 
 /**
  * Express configuration.
  */
-app.set('host', '0.0.0.0')
-app.set('port', process.env.PORT || 3030)
-app.set('json spaces', 2) // number of spaces for indentation
-app.use(cors())
-app.use(bodyParser.json())
-app.use(passport.initialize())
-app.use(passport.session())
+app.set("host", "0.0.0.0");
+app.set("port", process.env.PORT || 3030);
+app.set("json spaces", 2); // number of spaces for indentation
+app.use(cors());
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.post('/login', userController.postLogin)
-app.post('/signup', userController.postSignup)
-app.get('/webhook', userController.getWebhook)
-app.get('/jwks', userController.getJwks)
+app.post("/login", userController.postLogin);
+app.post("/signup", userController.postSignup);
+app.get("/webhook", userController.getWebhook);
+app.get("/jwks", userController.getJwks);
 /**
  * Start Express server.
  */
-app.listen(app.get('port'), () => {
+app.listen(app.get("port"), () => {
   console.log(
-    '⚡️ Ready on http://localhost:%d in %s mode',
-    app.get('port'),
-    app.get('env')
-  )
-})
+    "⚡️ Ready on http://localhost:%d in %s mode",
+    app.get("port"),
+    app.get("env")
+  );
+});
 
-module.exports = app
+module.exports = app;
